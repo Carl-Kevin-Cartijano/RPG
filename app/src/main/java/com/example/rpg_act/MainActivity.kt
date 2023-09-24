3b90e33d561fb3ae37150c1d1f7f96ea495333e5
Kaneki_Ken 05 <ccartijano@gmail.com>
2023-09-24 15:30:05 +0800

    1

--- app/src/main/java/com/example/rpg_act/MainActivity.kt
+++ app/src/main/java/com/example/rpg_act/MainActivity.kt
@@ -15,15 +15,15 @@ class MainActivity : AppCompatActivity() {
     private lateinit var attackButton: Button
     private lateinit var defendButton: Button
     private lateinit var healButton: Button
-    private lateinit var resetButton: Button
     private lateinit var rollDiceButton: Button
+    private lateinit var resetButton: Button
     private lateinit var gameStatusTextView: TextView
     private var playerHealth = 100
     private var enemyHealth = 100
     private var isPlayerDefending = false
     private var currentTurn: String? = null
-    private var gameOver = false // Flag to track game over
-    private var isFirstTurn = true // Flag to track the first turn
+    private var gameOver = false
+    private var isFirstTurn = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,23 +36,23 @@ class MainActivity : AppCompatActivity() {
         attackButton = findViewById(R.id.attackButton)
         defendButton = findViewById(R.id.defendButton)
         healButton = findViewById(R.id.healButton)
-        resetButton = findViewById(R.id.resetButton)
         rollDiceButton = findViewById(R.id.rollDiceButton)
+        resetButton = findViewById(R.id.resetButton)
         gameStatusTextView = findViewById(R.id.gameStatus)
 
         updateHealthViews()
         checkGameStatus()
 
         attackButton.setOnClickListener {
-            playerTurn()
+            playerTurn("Attack")
         }
 
         defendButton.setOnClickListener {
-            playerDefend()
+            playerTurn("Defend")
         }
 
         healButton.setOnClickListener {
-            playerHeal()
+            playerTurn("Heal")
         }
 
         resetButton.setOnClickListener {
@@ -60,16 +60,10 @@ class MainActivity : AppCompatActivity() {
         }
 
         rollDiceButton.setOnClickListener {
-            if (isFirstTurn) {
-                isFirstTurn = false
-                disableButtonsExcept(rollDiceButton)
-                rollDiceToDetermineFirstTurn()
-            }
+            rollDice()
         }
 
-
         disableButtons()
-
 
         rollDiceToDetermineFirstTurn()
     }
@@ -82,6 +76,13 @@ class MainActivity : AppCompatActivity() {
         rollDiceButton?.isEnabled = true
     }
 
+    private fun rollDice() {
+
+        val result = (1..6).random()
+        val rollDiceButton = findViewById<TextView>(R.id.rollDiceButton)
+        rollDiceButton.text = "You rolled: $result"
+    }
+
     private fun rollDice(sides: Int): Int {
         return Random().nextInt(sides) + 1
     }
@@ -90,28 +91,44 @@ class MainActivity : AppCompatActivity() {
         val diceResult = rollDice(2)
         if (diceResult == 1) {
             currentTurn = "Player"
-        }
-        else {
+        } else {
             currentTurn = "Enemy"
         }
         displayCurrentTurn()
         enableButtons()
+        if (currentTurn == "Enemy") {
+            enemyTurn()
+        }
     }
 
     private fun displayCurrentTurn() {
         gameStatusTextView.text = "Current Turn: $currentTurn"
     }
 
-    private fun playerTurn() {
-
+    private fun playerTurn(action: String) {
         if (!gameOver && currentTurn == "Player") {
             disableButtons() // Disable buttons at the start of the player's turn
             displayPlayerAction("It's your turn.")
-            displayPlayerAction("You use a normal attack.")
-            val damageDealt = rollDice(20) + 10
-            enemyHealth -= damageDealt
-            if (enemyHealth < 0) {
-                enemyHealth = 0
+            when (action) {
+                "Attack" -> {
+                    displayPlayerAction("You use a normal attack.")
+                    val damageDealt = rollDice(20) + 10
+                    enemyHealth -= damageDealt
+                    if (enemyHealth < 0) {
+                        enemyHealth = 0
+                    }
+                }
+                "Defend" -> {
+                    isPlayerDefending = true
+                    displayPlayerAction("You defend against the enemy's attack.")
+                }
+                "Heal" -> {
+                    if (playerHealth < 100) {
+                        val healingAmount = rollDice(15) + 10
+                        playerHealth += healingAmount
+                        displayPlayerAction("You heal for $healingAmount health.")
+                    }
+                }
             }
             updateHealthViews()
             checkGameStatus()
@@ -121,61 +138,38 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun playerDefend() {
-        isPlayerDefending = true
-        displayPlayerAction("You defend against the enemy's attack.")
-        currentTurn = "Enemy"
-        displayCurrentTurn()
-        enemyTurn()
-    }
-
-    private fun playerHeal() {
-        if (!gameOver && playerHealth < 100) {
-            val healingAmount = rollDice(15) + 10
-            playerHealth += healingAmount
-            displayPlayerAction("You heal for $healingAmount health.")
-            currentTurn = "Enemy"
-            displayCurrentTurn()
-            enemyTurn()
-        }
-    }
-
     private fun enemyTurn() {
-
         if (!gameOver && currentTurn == "Enemy") {
             disableButtons()
             val action = rollDice(3)
-            if (action == 1) {
-                val damageDealt = if (isPlayerDefending) {
-                    rollDice(10) + 5
-                } else {
-                    rollDice(15) + 10
+            when (action) {
+                1 -> {
+                    val damageDealt = if (isPlayerDefending) {
+                        rollDice(10) + 5
+                    } else {
+                        rollDice(15) + 10
+                    }
+                    playerHealth -= damageDealt
+                    if (playerHealth < 0) {
+                        playerHealth = 0
+                    }
+                    isPlayerDefending = false
+                    displayEnemyAction("Enemy attacks for $damageDealt damage.")
                 }
-                playerHealth -= damageDealt
-                if (playerHealth < 0) {
-                    playerHealth = 0
+                2 -> {
+                    displayEnemyAction("Enemy defends.")
                 }
-                isPlayerDefending = false
-                updateHealthViews()
-                checkGameStatus()
-                currentTurn = "Player"
-                displayCurrentTurn()
-                displayEnemyAction("Enemy attacks for $damageDealt damage.")
-                enableButtons()
-            } else if (action == 2) {
-                displayEnemyAction("Enemy defends.")
-                currentTurn = "Player"
-                displayCurrentTurn()
-                enableButtons()
-            } else {
-                val healing = rollDice(15) + 5
-                enemyHealth += healing
-                updateHealthViews()
-                displayCurrentTurn()
-                displayEnemyAction("Enemy heals for $healing health.")
-                currentTurn = "Player"
-                enableButtons()
+                3 -> {
+                    val healing = rollDice(15) + 5
+                    enemyHealth += healing
+                    displayEnemyAction("Enemy heals for $healing health.")
+                }
             }
+            updateHealthViews()
+            checkGameStatus()
+            currentTurn = "Player"
+            displayCurrentTurn()
+            enableButtons()
         }
     }
 
@@ -190,8 +184,7 @@ class MainActivity : AppCompatActivity() {
             disableAllButtonsExceptReset()
             if (playerHealth <= 0) {
                 "Game over. You lose.".also { gameStatusTextView.text = it }
-            }
-            else {
+            } else {
                 "Congratulations! You win.".also { gameStatusTextView.text = it }
             }
         }
@@ -207,10 +200,10 @@ class MainActivity : AppCompatActivity() {
         attackButton.isEnabled = true
         defendButton.isEnabled = true
         healButton.isEnabled = true
+        rollDiceButton.isEnabled = true
         if (currentTurn == "Player") {
             attackButton.isEnabled = !gameOver
         }
-        rollDiceButton.isEnabled = true
     }
 
     private fun disableAllButtonsExceptReset() {
@@ -238,5 +231,10 @@ class MainActivity : AppCompatActivity() {
         gameStatusTextView.text = ""
         currentTurn = "Player"
         displayCurrentTurn()
+
+
+        val rollDiceButton = findViewById<TextView>(R.id.rollDiceButton)
+        rollDiceButton.text = "Roll Dice"
     }
+
 }
